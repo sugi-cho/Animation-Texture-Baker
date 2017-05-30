e2d3f945d01a63119173e10760ba836b028f2c6d
HironoriSugino <github@sugi.cc>
2017-05-30 18:22:59 +0900

    change all

---
+++ Assets/AnimationBaker/Scripts/AnimationTextureBaker.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using System.IO;
+#endif
+
+public class AnimationTextureBaker : MonoBehaviour
+{
+    public ComputeShader infoTexGen;
+
+    public struct VertInfo
+    {
+        public Vector3 position;
+        public Vector3 normal;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        var animation = GetComponent<Animation>();
+        var skin = GetComponentInChildren<SkinnedMeshRenderer>();
+        var vCount = skin.sharedMesh.vertexCount;
+        var texWidth = Mathf.NextPowerOfTwo(vCount);
+        var mesh = new Mesh();
+
+        foreach (AnimationState state in animation)
+        {
+            animation.Play(state.name);
+            var frames = Mathf.NextPowerOfTwo((int)(state.length / 0.05f));
+            var dt = state.length / frames;
+            var time = 0f;
+            var infoList = new List<VertInfo>();
+
+            var pRt = new RenderTexture(texWidth, frames, 0, RenderTextureFormat.ARGBHalf);
+            pRt.name = string.Format("{0}.{1}.posTex", name, state.name);
+            var nRt = new RenderTexture(texWidth, frames, 0, RenderTextureFormat.ARGBHalf);
+            nRt.name = string.Format("{0}.{1}.normTex", name, state.name);
+            foreach (var rt in new[] { pRt, nRt })
+            {
+                rt.enableRandomWrite = true;
+                rt.Create();
+                RenderTexture.active = rt;
+                GL.Clear(true, true, Color.clear);
+            }
+
+            for (var i = 0; i < frames; i++)
+            {
+                state.time = time;
+                animation.Sample();
+                skin.BakeMesh(mesh);
+
+                infoList.AddRange(Enumerable.Range(0, vCount)
+                    .Select(idx => new VertInfo()
+                    {
+                        position = mesh.vertices[idx],
+                        normal = mesh.normals[idx]
+                    })
+                );
+
+                time += dt;
+            }
+            var buffer = new ComputeBuffer(infoList.Count, System.Runtime.InteropServices.Marshal.SizeOf(typeof(VertInfo)));
+            buffer.SetData(infoList.ToArray());
+
+            var kernel = infoTexGen.FindKernel("CSMain");
+            uint x, y, z;
+            infoTexGen.GetKernelThreadGroupSizes(kernel, out x, out y, out z);
+
+            infoTexGen.SetInt("VertCount", vCount);
+            infoTexGen.SetBuffer(kernel, "Info", buffer);
+            infoTexGen.SetTexture(kernel, "OutPosition", pRt);
+            infoTexGen.SetTexture(kernel, "OutNormal", nRt);
+            infoTexGen.Dispatch(kernel, vCount / (int)x + 1, frames / (int)y + 1, 1);
+
+            buffer.Release();
+
+#if UNITY_EDITOR
+            var folderName = "BakedAnimationTex";
+            var folderPath = Path.Combine("Assets", folderName);
+            if (!AssetDatabase.IsValidFolder(folderPath))
+                AssetDatabase.CreateFolder("Assets", folderName);
+
+            var subFolder = name;
+            var subFolderPath = Path.Combine(folderPath, subFolder);
+            if (!AssetDatabase.IsValidFolder(subFolderPath))
+                AssetDatabase.CreateFolder(folderPath, subFolder);
+
+            var posTex = RenderTextureToTexture2D.Convert(pRt);
+            var normTex = RenderTextureToTexture2D.Convert(nRt);
+            Graphics.CopyTexture(pRt, posTex);
+            Graphics.CopyTexture(nRt, normTex);
+
+            AssetDatabase.CreateAsset(posTex, Path.Combine(subFolderPath, pRt.name + ".asset"));
+            AssetDatabase.CreateAsset(normTex, Path.Combine(subFolderPath, nRt.name + ".asset"));
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+#endif
+        }
+    }
+}
