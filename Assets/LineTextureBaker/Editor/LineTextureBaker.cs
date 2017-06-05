cec8861d88f757f9b9f21ee300f823e76b78cec7
HironoriSugino <github@sugi.cc>
2017-06-05 19:48:31 +0900

    add MeshToLineTexture Converter

---
+++ Assets/LineTextureBaker/Editor/LineTextureBaker.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+
+using System.IO;
+
+
+public class LineTextureBaker : EditorWindow
+{
+    public struct LineInfo
+    {
+        public LineInfo(int i0, int i1)
+        {
+            if (i0 < i1)
+            {
+                idx0 = i0;
+                idx1 = i1;
+                return;
+            }
+            idx0 = i1;
+            idx1 = i0;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null || this.GetType() != obj.GetType())
+                return false;
+
+            var info = (LineInfo)obj;
+            return idx0 == info.idx0 && idx1 == info.idx1;
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        public int idx0;
+        public int idx1;
+    }
+
+    [MenuItem("Window/LineTextureBaker")]
+    public static void CreateWindow()
+    {
+        GetWindow<LineTextureBaker>();
+    }
+
+    public Mesh targetMesh;
+    public ComputeShader lineTexGen;
+    RenderTexture output;
+
+    private void OnGUI()
+    {
+        targetMesh = (Mesh)EditorGUILayout.ObjectField("mesh", targetMesh, typeof(Mesh), false);
+        lineTexGen = (ComputeShader)EditorGUILayout.ObjectField("lineTexGenCS", lineTexGen, typeof(ComputeShader), false);
+        if (GUILayout.Button("Bake Texture"))
+            BakeTexture(targetMesh);
+        if (GUILayout.Button("Create Mesh"))
+            CreateMesh();
+    }
+
+    void BakeTexture(Mesh mesh)
+    {
+        var vs = mesh.vertices;
+        var tris = mesh.triangles;
+        var lines = new List<LineInfo>();
+
+        for (var i = 0; i < tris.Length / 3; i++)
+        {
+            var idx0 = tris[i * 3 + 0];
+            var idx1 = tris[i * 3 + 1];
+            var idx2 = tris[i * 3 + 2];
+
+            var l0 = new LineInfo(idx0, idx1);
+            var l1 = new LineInfo(idx1, idx2);
+            var l2 = new LineInfo(idx2, idx0);
+            if (!lines.Contains(l0))
+                lines.Add(l0);
+            if (!lines.Contains(l1))
+                lines.Add(l1);
+            if (!lines.Contains(l2))
+                lines.Add(l2);
+        }
+        var ls = lines.ToArray();
+        var vBuffer = new ComputeBuffer(vs.Length, System.Runtime.InteropServices.Marshal.SizeOf(typeof(Vector3)));
+        var lBuffer = new ComputeBuffer(ls.Length, System.Runtime.InteropServices.Marshal.SizeOf(typeof(LineInfo)));
+        vBuffer.SetData(vs);
+        lBuffer.SetData(ls);
+
+        output = new RenderTexture(Mathf.NextPowerOfTwo(ls.Length), 2, 0, RenderTextureFormat.ARGBHalf);
+        output.enableRandomWrite = true;
+        output.Create();
+
+        var kernel = lineTexGen.FindKernel("CSMain");
+        lineTexGen.SetBuffer(kernel, "_VBuffer", vBuffer);
+        lineTexGen.SetBuffer(kernel, "_LBuffer", lBuffer);
+        lineTexGen.SetTexture(kernel, "Output", output);
+        uint x, y, z;
+        lineTexGen.GetKernelThreadGroupSizes(kernel, out x, out y, out z);
+        lineTexGen.Dispatch(kernel, (int)(ls.Length / x) + 1, 1, 1);
+
+        vBuffer.Release();
+        lBuffer.Release();
+
+        var folderName = "BakedLineTex";
+        var folderPath = Path.Combine("Assets", folderName);
+        if (!AssetDatabase.IsValidFolder(folderPath))
+            AssetDatabase.CreateFolder("Assets", folderName);
+
+        var tex2d = RenderTextureToTexture2D.Convert(output);
+        AssetDatabase.CreateAsset(tex2d, Path.Combine(folderPath, mesh.name + string.Format("_line.{0}.asset", ls.Length)));
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+
+        Selection.activeObject = tex2d;
+    }
+
+    public static void CreateMesh()
+    {
+        var mesh = new Mesh();
+        var numPoints = 65000;
+        mesh.vertices = Enumerable.Repeat(Vector3.zero, numPoints).ToArray();
+        mesh.uv = Enumerable.Range(0, numPoints).Select(i => new Vector2((float)i, (float)i / (float)numPoints)).ToArray();
+        mesh.SetIndices(Enumerable.Range(0, numPoints).ToArray(), MeshTopology.Points, 0);
+
+        AssetDatabase.CreateAsset(mesh, "Assets/point65000.asset");
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+
+        Selection.activeObject = mesh;
+    }
+}
---
+++ Assets/LineTextureBaker/Shaders/LineVisualize.shader
@@ -0,0 +1,61 @@
+﻿Shader "Unlit/LineVisualize"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_LineTex("line tex", 2D) = "vlack"{}
+		_LCount ("num lines", Float) = 5000
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+			#define ts _LineTex_TexelSize
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex, _LineTex;
+			float4 _LineTex_TexelSize;
+			float _LCount;
+			
+			v2f vert (appdata v)
+			{
+				float count = frac(v.uv.y + _Time.x*0.001) * _LCount;
+
+				float x = (floor(count) + 0.5) * ts.x;
+				float y = frac(count);
+				
+				v.vertex = tex2Dlod(_LineTex, float4(x,y,0,0));
+
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				return 1;
+			}
+			ENDCG
+		}
+	}
+}
