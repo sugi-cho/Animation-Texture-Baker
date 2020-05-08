bc264ca0af1bcc2c64d096e2fbd45bbb9fe445d0
sugino <git@sugi.cc>
2020-05-09 04:40:15 +0900

    vertex animation shader graph

---
+++ Assets/ShaderGraph/Scripts/VertIdxToUv.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class VertIdxToUv : MonoBehaviour
+{
+    public int channel = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var mesh = GetComponent<MeshFilter>().mesh;
+        mesh.hideFlags = HideFlags.DontSave;
+        var uv = new List<Vector2>();
+        for (var i = 0; i < mesh.vertexCount; i++)
+            uv.Add(new Vector2(i + 0.5f, 0));
+        mesh.SetUVs(channel, uv);
+    }
+}
